ecb5c987923f61e43e89212070c13c9b1520f894
CypherIgnis <116596423+CypherIgnis@users.noreply.github.com>
2022-11-14 16:41:26 -0300

    Create student.py

---
+++ student.py
@@ -0,0 +1,36 @@
+# student
+class Student():
+    #atributos#
+    id=
+    name=
+    middle_name:
+    surname:
+    #fin seccion atributos#
+
+    def __init__(self,,):
+        print("Comenzando programa")
+        self.id
+        self.name=
+        self.middle_name:
+        self.surname:
+
+    def printt(self):
+        print(self.,"/",self.)
+
+
+
+o=Fraction(3,4)
+print(o.numerator)
+print(o.denominator)
+a=Fraction(3,2)
+b=Fraction(6,5)
+a.printt()
+b.printt()
+p=b.plus(2)
+print(p)
+m=a.minus(2)
+print(m)
+x=b.multiplication(4)
+print(x)
+d=a.division(3)
+print(d)
